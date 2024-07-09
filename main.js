146e9ebf815966bfbb91ed8c2ee856cfa184cce2
ihell <ihell269@gmail.com>
2024-07-10 00:12:02 +0700

    mengubah fungsi tombol manjadi hadir siswa

---
+++ main.js
@@ -0,0 +1,65 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js";
+import {
+  getFirestore,
+  collection,
+  addDoc,
+  getDocs,
+  updateDoc,
+  doc,
+  query,
+  orderBy
+} from "https://www.gstatic.com/firebasejs/10.8.0/firebase-firestore.js";
+
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyB8g6kCR8laDnH1YCF9cYVy10lF1y7s2i0",
+  authDomain: "bakwan-jagung.firebaseapp.com",
+  projectId: "bakwan-jagung",
+  storageBucket: "bakwan-jagung.appspot.com",
+  messagingSenderId: "710653450064",
+  appId: "1:710653450064:web:5cbf9ff4240922b03ffe55",
+  measurementId: "G-VBHD7G08PJ"
+};
+
+// Inisialisasi Firebase
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+export async function ambilDaftarAbsensi() {
+  const refDokumen = collection(db, "absen");
+  const kueri = query(refDokumen, orderBy("nama"));
+  const cuplikanKueri = await getDocs(kueri);
+
+  let hasil = [];
+  cuplikanKueri.forEach((dok) => {
+    hasil.push({
+      id: dok.id,
+      nama: dok.data().nama,
+      kelamin: dok.data().kelamin,
+      kelas: dok.data().kelas,
+      keterangan: dok.data().keterangan
+    });
+  });
+
+  return hasil;
+}
+
+export async function ubahStatusAbsensi(docId, keterangan) {
+  await updateDoc(doc(db, "absen", docId), {
+    keterangan: keterangan
+  });
+}
+
+export async function tambahAbsensi(nama, kelamin, kelas) {
+  try {
+    const dokRef = await addDoc(collection(db, 'absen'), {
+      nama: nama,
+      kelamin: kelamin,
+      kelas: kelas,
+      keterangan: ''
+    });
+    console.log('Berhasil menambah ' + dokRef.id);
+  } catch (e) {
+    console.log('Gagal menambah ' + e);
+  }
+}
